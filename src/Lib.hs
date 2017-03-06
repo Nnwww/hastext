29ba6d1e5a6f163077bcdd2fe2df994a464d6b47
Nnwww <johndororo@gmail.com>
2017-03-07 00:20:43 +0900

    begin to use haddock

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,30 +12,33 @@ import qualified Data.ByteString as BS
 import           Data.Store (Store, encode)
 import           TH.Derive (Deriving, derive)
 
+-- | Arguments necessary to learn
 type Args = (Method, Options)
 
+-- | Learning algorithms
 data Method
   = Cbow
   | Skipgram
   deriving Show
 
+-- | Global options to learn
 data Options = Options
-  { input          :: FilePath
-  , output         :: FilePath
-  , lr             :: Double
-  , lrUpdateTokens :: Int
-  , dim            :: Int
-  , windows        :: Int
-  , epoch          :: Int
-  , minCount       :: Int
-  , negatives      :: Int
-  , loss           :: Loss
-  , tSub           :: Double
-  , threads        :: Int
-  , verbose        :: Int
+  { input          :: FilePath -- ^ training file path
+  , output         :: FilePath -- ^ output file path
+  , lr             :: Double   -- ^ learning rate
+  , lrUpdateTokens :: Int      -- ^ the number of tokens that update the learning rate
+  , dim            :: Int      -- ^ size of word vectors
+  , windows        :: Int      -- ^ size of the context window
+  , epoch          :: Int      -- ^ number of epochs
+  , minCount       :: Int      -- ^ minimal number of word occurences
+  , negatives      :: Int      -- ^ number of negatives sampled
+  , loss           :: Loss     -- ^ loss function {ns, hs}
+  , tSub           :: Double   -- ^ sub-sampling threshold
+  , threads        :: Int      -- ^ number of threads
+  , verbose        :: Int      -- ^ verbosity level
   } deriving (Show)
 
--- Loss functions
+-- | Loss functions
 data Loss = Negative | Hierarchical deriving (Show, Read)
 
 -- derive Store instances at compile time
@@ -51,9 +54,11 @@ $($(derive [d|
     instance Deriving (Store Loss)
     |]))
 
+-- | Start training
 train :: Args -> IO (FilePath)
 train = undefined
 
+-- | Save Args
 saveArgs :: Args -> IO ()
 saveArgs args@(_, opt) = BS.writeFile outFile $ encode args
   where
