22905ae2c11fc7438ed72ff223d6cacd2aeb51ae
Nnwww <johndororo@gmail.com>
2017-04-18 07:17:16 +0900

    implement Dict module roughly.

--- src/WordEmbedding/FastText/Dict.hs
+++ src/WordEmbedding/FastText/Dict.hs
@@ -7,43 +7,30 @@ module WordEmbedding.FastText.Dict where
 
 import           WordEmbedding.FastText.Args
 
-import           Control.Monad.Primitive (PrimMonad, PrimState)
-import           Control.Monad.ST
-import           Control.Monad.Trans.Resource
-import           Control.Applicative
+import           System.IO                    as SI
+import qualified System.Random.MWC            as RM
 
 import qualified Data.Char                    as C
-import           Data.Word
-import qualified Data.List                    as L
-import qualified Data.Map.Strict              as M
-import           Data.Maybe
+import qualified Data.Map.Strict              as MS
+import           Data.Map.Strict              ((!))
 import qualified Data.ByteString              as BS
 import qualified Data.Vector                  as V
-import qualified Data.Vector.Mutable          as VM
-import qualified Data.Vector.Unboxed          as VU
-import qualified Data.Vector.Unboxed.Mutable  as VUM
-import qualified Data.Vector.Generic.Mutable  as VGM
 import qualified Data.Text                    as T
-import qualified Data.Store                   as S
 import qualified Data.Hashable                as H
 import           Data.Conduit
 import qualified Data.Conduit.Combinators     as CC
+
+import qualified Data.Store                   as S
 import           TH.Derive (Deriving, derive)
 
-
-type WordId    = Int
-type TMap a    = M.Map T.Text a
+type TMap a    = MS.Map T.Text a
 type Vec a     = V.Vector a
-type MVec s a  = VM.MVector s a
-type UVec a    = VU.Vector a
-type UMVec s a = VUM.MVector s a
-type TrnsMV prim v a = prim (v (PrimState prim) a)
 
 data Entry = Entry
   { eword    :: T.Text
   , count    :: Word
-  , etype    :: EntryType
-  -- , subwords :: ~(Vec WordId)
+  -- , etype    :: EntryType
+  -- , subwords :: ~(Vec T.Text)
   }
 
 data EntryType = EWord | ELabel
@@ -80,41 +67,60 @@ loadDict :: FilePath -> IO Dict
 loadDict readPath = S.decodeIO =<< BS.readFile readPath
 
 initDiscards :: Double -> TMap Entry -> Word -> TMap Double
-initDiscards tsub ents tks = M.map calcDiscard ents
+initDiscards tsub ents tks = MS.map calcDiscard ents
   where
     calcDiscard e =
       let f = realToFrac (count e) / realToFrac tks in
         (sqrt $ tsub / f) + (tsub / f)
 
+-- |
+-- The function folding words splited by @Data.Char.isSpace@ from a file.
+-- Note that The file as source is utf8 only.
+wordsFromFile :: (r -> T.Text -> r) -> r -> FilePath -> IO r
+wordsFromFile modifier plain readPath =
+  runConduitRes $ CC.sourceFile readPath
+  .| CC.decodeUtf8
+  .| CC.splitOnUnboundedE C.isSpace
+  .| CC.foldl modifier plain
+
+discard :: TMap Double -> RM.GenIO -> T.Text -> IO Bool
+discard diss rand word = do
+  randProb <- RM.uniform rand
+  let disProb = diss ! word in
+    return $ randProb > disProb
+
+getLine :: Handle -> Dict -> RM.GenIO -> IO (Vec Entry)
+getLine h (Dict{entries = ents, discards = diss}) rand =
+  runConduit $ CC.sourceHandle h
+  .| CC.decodeUtf8
+  .| CC.takeWhileE (/= '\n')
+  .| CC.splitOnUnboundedE C.isSpace
+  .| CC.filterM (discard diss rand)
+  .| CC.map (\w -> ents ! w) -- The (!) symbol can not apply partially. It probably conflicts BangPattern etc.
+  .| CC.sinkVector
 
 initFromFile :: Args -> IO Dict
-initFromFile (_, Options {input = inp, tSub = tsub, minCount = minc}) = do
-  ents <- runConduitRes $ wordsFromFile addEnts M.empty inp
-  let (newEnts, newTks) = threshold minc ents
-      newDis = initDiscards tsub newEnts newTks in
-    return $ Dict newEnts newDis newTks
-  where
-    wordsFromFile modifier plain readPath =
-      CC.sourceFile readPath
-      .| CC.decodeUtf8
-      .| CC.splitOnUnboundedE C.isSpace
-      .| CC.foldl modifier plain
+initFromFile (_, Options{input = inp, tSub = tsub, minCount = minc}) = do
+  ents <- wordsFromFile addEnts MS.empty inp
+  let newEnts = threshold ents minc
+      newTkns = sizeTokens newEnts
+      newDiss = initDiscards tsub newEnts newTkns in
+    return $ Dict newEnts newDiss newTkns
 
 hashMod :: T.Text -> Int -> Int
 hashMod str bound = H.hash str `mod` bound
 
-threshold :: Word -> TMap Entry -> (TMap Entry, Word)
-threshold t ents = (newEnts, sizeTokens ents)
-  where
-    newEnts = M.filter (\e -> t > count e) ents -- Improvable?: if lazy IO, it can suppress explosion of memory usage here.
+threshold :: TMap Entry -> Word -> TMap Entry
+threshold ents t = MS.filter (\e -> t > count e) ents
+    -- Improvable?: if I use lazy IO, it can suppress explosion of memory usage here.
 
 sizeTokens :: TMap Entry -> Word
 sizeTokens ents = foldr (\e acc -> acc + count e) 0 ents
 
 addEnts :: TMap Entry -> T.Text -> TMap Entry
-addEnts ents t = M.alter newEntry t ents
+addEnts ents t = MS.alter newEntry t ents
   where
-    newEntry (Just (old@Entry {count = c})) = Just $ old {count = succ c}
-    newEntry Nothing = Just $ Entry {eword = t, count = 1, etype = EWord}
+    newEntry (Just old@Entry{count = c}) = Just $ old {count = succ c}
+    newEntry Nothing = Just $ Entry {eword = t, count = 1}
     -- todo: implement ngram and label functionality
     -- nGrams n = (!! n) . L.transpose . L.map T.inits . T.tails
