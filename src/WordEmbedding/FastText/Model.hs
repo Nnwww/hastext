a54993c64ded7f7604cc2cce1fe2a02b312b53c5
Nnwww <johndororo@gmail.com>
2017-04-25 01:24:21 +0900

    add memorized log function

--- src/WordEmbedding/FastText/Model.hs
+++ src/WordEmbedding/FastText/Model.hs
@@ -13,11 +13,27 @@ sigmoid tableSize maxValue x =
     doubledTableSize = fromIntegral tableSize :: Double
 
     mapInputToTable :: Double -> Word
-    mapInputToTable x' = round $ ((x' + maxValue) * doubledTableSize / maxValue / 2.0)
+    mapInputToTable lx = floor $ ((lx + maxValue) * doubledTableSize / maxValue / 2.0)
 
     sigmoidTable :: AU.UArray Word Double
     sigmoidTable = AU.listArray (0, tableSize)
       [lsigmoid . mapIndexToTable $ i | i <- [0.0 .. doubledTableSize]]
 
+    mapIndexToTable :: Double -> Double
     mapIndexToTable idx = (idx * 2.0 * maxValue) / doubledTableSize - maxValue
     lsigmoid lx = 1.0 / (1.0 + (exp $ -lx))
+
+-- memorized log function.
+log :: Word -> (Double -> Double)
+log tableSize x =
+  if 1.0 < x then 0.0
+  else logTable AU.! (mapInputToTable x)
+  where
+    doubledTableSize = fromIntegral tableSize :: Double
+
+    mapInputToTable :: Double -> Word
+    mapInputToTable lx = floor $ (lx * doubledTableSize)
+
+    logTable :: AU.UArray Word Double
+    logTable = AU.listArray (0, tableSize)
+      [Prelude.log $ ((i + 1e-5) / doubledTableSize) | i <- [0.0 .. doubledTableSize]]
