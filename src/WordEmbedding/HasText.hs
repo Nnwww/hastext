56c207428d0f804a42d0a891626545bae7675b80
Nnwww <johndororo@gmail.com>
2017-05-26 19:41:16 +0900

    update skipGram to monad transformer style

--- src/WordEmbedding/HasText.hs
+++ src/WordEmbedding/HasText.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module WordEmbedding.HasText where
 
 import qualified WordEmbedding.HasText.Args  as HA
@@ -11,7 +12,6 @@ import qualified System.Random.MWC            as RM
 import qualified System.IO                    as SI
 
 import Control.Concurrent
-import Control.Monad.ST
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.Reader
@@ -24,42 +24,39 @@ windowRange line model targetIdx = do
   let winFrom = if targetIdx - winRange > 0 then targetIdx - winRange else 0
       winTo   = if V.length line > targetIdx + winRange then targetIdx + winRange else V.length line - 1
       inWindowAndNotTarget i _ = winFrom < i && i < winTo && i /= targetIdx
-  return $ V.ifilter inWindowAndNotTarget line
+  return $ V.ifilter (\i e -> not $ inWindowAndNotTarget i e) line
 
 skipGram ::  Double -> V.Vector T.Text -> ReaderT HM.Params (StateT HM.Model HM.MVarIO) ()
-skipGram lr line = updateEachWinElems . V.zip (V.fromList [0..]) $ line
+skipGram lr line = forM_ [0..V.length line] $ \idx -> do
+  model <- lift get
+  params <- ask
+  updateRange <- liftIO $ runReaderT (windowRange line model idx) params
+  mapM_ (learn $ V.unsafeIndex line idx) updateRange
   where
-    updateEachWinElems idxAndWords = do
-      let h = V.head
-      updateRange <- windowRange line m targetIdx
-      V.foldM (learn updTarget) m updateRange
-    learn target acc e = do
-      acc
-      HM.update (V.singleton target) e lr
+    learn input target = HM.update (V.singleton input) target lr
 
 
-
-cbow :: HM.Model -> Double -> V.Vector T.Text -> IO HM.Model
-cbow model lr line = V.ifoldM update model line
-  where
-    update m targetIdx updTarget = do
-      inputRange <- windowRange line m targetIdx
-      HM.update m inputRange updTarget lr
+-- cbow :: HM.Model -> Double -> V.Vector T.Text -> IO HM.Model
+-- cbow model lr line = V.ifoldM update model line
+--   where
+--     update m targetIdx updTarget = do
+--       inputRange <- windowRange line m targetIdx
+--       HM.update m inputRange updTarget lr
 
 -- TODO: compare parallelization using MVar with one using ParIO.
-trainThread :: HM.Model -> MVar W.Word -> Integer -> IO ()
-trainThread model tokenCountRef threadNo = do
-  h <- SI.openFile inputPath SI.ReadMode
-  size <- SI.hFileSize h
-  SI.hSeek h SI.AbsoluteSeek (size * threadNo `quot` threads)
-
-  SI.hClose h
-  where
-    inputPath = HA.input . snd $ args
-    threads = fromIntegral . HA.threads . snd $ args
-    ntokens = HD.ntokens . HM.dict $ model
-    epoch = undefined
-    trainLoop  = undefined
+-- trainThread :: HM.Model -> MVar W.Word -> Integer -> IO ()
+-- trainThread model tokenCountRef threadNo = do
+--   h <- SI.openFile inputPath SI.ReadMode
+--   size <- SI.hFileSize h
+--   SI.hSeek h SI.AbsoluteSeek (size * threadNo `quot` threads)
+--
+--   SI.hClose h
+--   where
+--     inputPath = HA.input . snd $ args
+--     threads = fromIntegral . HA.threads . snd $ args
+--     ntokens = HD.ntokens . HM.dict $ model
+--     epoch = undefined
+--     trainLoop  = undefined
 
 
 -- TODO: write test code using simpler corpuses, and then try to compare hastext's result with gensim's result.
