6b670cd289553ecf472a7ca2172eb6256112ce9a
Nnwww <johndororo@gmail.com>
2017-07-19 17:57:13 +0900

    fix TestData

--- test/TestData.hs
+++ test/TestData.hs
@@ -5,7 +5,7 @@ import           Paths_hastext
 import           Data.Monoid
 import           Data.List                  as L
 import           Data.Text                  as T
-import qualified WordEmbedding.HasText.Args as HA
+import           WordEmbedding.HasText.Args
 
 noFailInputList :: [Text]
 noFailInputList = mconcat . L.map (L.replicate 5 . T.singleton) $ ['a' .. 'e']
@@ -14,42 +14,43 @@ noFailPath, text8Path :: IO FilePath
 noFailPath = getDataFileName "data/NonFail.txt"
 text8Path  = getDataFileName "data/text8s/text8_1k"
 
-noFailParams, noFailOnMultiThreadParams, text8RunParams :: IO HA.Args
+noFailParams, noFailOnMultiThreadParams, text8RunParams :: IO HasTextArgs
 noFailParams = do
   inputFilePath <- noFailPath
-  return (HA.Skipgram, noFailDefault{ HA.input  = inputFilePath
-                                    , HA.output = inputFilePath <> ".out"})
+  return (Skipgram, noFailDefault{ _input  = inputFilePath
+                                 , _output = inputFilePath <> ".out"})
 noFailOnMultiThreadParams = do
   inputFilePath <- noFailPath
-  return (HA.Skipgram, noFailDefault{ HA.input   = inputFilePath
-                                    , HA.output  = inputFilePath <> ".out"
-                                    , HA.threads = 4})
+  return (Skipgram, noFailDefault{ _input   = inputFilePath
+                                 , _output  = inputFilePath <> ".out"
+                                 , _threads = 4})
 text8RunParams = do
   inputFilePath <- text8Path
-  return (HA.Skipgram, text8RunDefault{ HA.input  = inputFilePath
-                                      , HA.output = inputFilePath <> ".out"})
+  return (Skipgram, text8RunDefault{ _input  = inputFilePath
+                                   , _output = inputFilePath <> ".out"})
 
-noFailDefault,text8RunDefault :: HA.Options
-noFailDefault =  HA.Options
-  { HA.input          = ""
-  , HA.output         = ""
-  , HA.lr             = 0.05
-  , HA.lrUpdateTokens = 100
-  , HA.dim            = 100
-  , HA.windows        = 2
-  , HA.epoch          = 5
-  , HA.minCount       = 1
-  , HA.negatives      = 5
-  , HA.loss           = HA.Negative
-  , HA.tSub           = 0.0001
-  , HA.threads        = 1
-  , HA.verbose        = 1
+noFailDefault,text8RunDefault :: HasTextOptions
+noFailDefault =  HasTextOptions
+  { _input          = ""
+  , _output         = ""
+  , _initLR         = 0.05
+  , _lrUpdateTokens = 100
+  , _dim            = 100
+  , _windows        = 2
+  , _epoch          = 5
+  , _minCount       = 1
+  , _negatives      = 5
+  , _lossFn         = Negative
+  , _tSub           = 0.0001
+  , _threads        = 1
+  , _verbose        = 1
   }
+
 text8RunDefault = noFailDefault
-  { HA.input    = ""
-  , HA.output   = ""
-  , HA.dim      = 100
-  , HA.windows  = 5
-  , HA.minCount = 5
-  , HA.threads  = 8
+  { _input    = ""
+  , _output   = ""
+  , _dim      = 100
+  , _windows  = 5
+  , _minCount = 5
+  , _threads  = 8
   }
