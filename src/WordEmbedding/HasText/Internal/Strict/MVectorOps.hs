d25936ae705579a114c267873ea464ebd639482d
Nnwww <johndororo@gmail.com>
2017-08-14 08:20:57 +0900

    add ghc-prof-options

--- src/WordEmbedding/HasText/Internal/Strict/MVectorOps.hs
+++ src/WordEmbedding/HasText/Internal/Strict/MVectorOps.hs
@@ -42,6 +42,7 @@ mapiM_ f ov = go 0
 
 foriM_ :: VUM.IOVector Double -> (Int -> Double -> IO Double) -> IO ()
 foriM_ ov f = mapiM_ f ov
+{-# INLINE foriM_ #-}
 
 foldiM :: VUM.IOVector Double -> a -> (Int -> a -> Double -> IO a) -> IO a
 foldiM ov a f = go 0 a
